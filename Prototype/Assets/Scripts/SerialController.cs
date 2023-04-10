aeb00b120c67f2099f8b63123c500ec4980cfc7d
Treyne11 <54631495+Treyne11@users.noreply.github.com>
2023-04-10 21:26:17 +1000

    Mid integration for the serial controller.
    Finished combining the serial files... Added some variables.
    
    WIP - Needs logic to properly integrate persistent health.

--- Prototype/Assets/Scripts/SerialController.cs
+++ Prototype/Assets/Scripts/SerialController.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
 
-public class arduinoSerial : MonoBehaviour
+public class SerialController : MonoBehaviour
 {
     //INITIALISE SERIAL VARIABLES
-    //Two ports, one for (S)prossen and one for (T)ree
-    SerialPort port_S; 
-    SerialPort port_T;
-    public string portName_S;
-    public string portName_T;
+    // Init 2 variables for serial data, one for (S)prossen and one for (T)ree
+    private SerialPort _portS;
+    private SerialPort _portT;
+    public string portNameS;
+    public string portNameT;
     public string[] ports;
 
     //INITIALISE OTHER VARIABLES
-    GameObject sprossen;
+    public GameObject sprossen;
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject backWall;
+    public GameObject floor;
+    public GameObject tree;
+    
+    // Init variable to track health of persistant interaction (Tree). 
     private int _soulHealth;
 
     // Start is called before the first frame update
@@ -27,32 +34,32 @@ public class arduinoSerial : MonoBehaviour
         //COPY AND PASTE TO SET FOR YOUR OWN COMPUTER, COMMENT OUT OTHERS
         //portName_S = "/dev/cu.usbmodem1413401";
         //portName_T = "/dev/cu.usbmodem1413301";
-        portName_S = "COM7";
-        portName_T = "COM6";
+        portNameS = "COM7";
+        portNameT = "COM6";
 
         //SETUP PORT
-        port_S = new SerialPort(portName_S, 9600);
-        port_T = new SerialPort(portName_T, 9600);
+        _portS = new SerialPort(portNameS, 9600);
+        _portT = new SerialPort(portNameT, 9600);
 
         //SET TIMEOUTS TO LOW NUMBERS TO MAKE COMMUNICATION FAST
-        port_S.ReadTimeout = 10;
-        port_S.WriteTimeout = 10;
-        port_S.DtrEnable = true;
-        port_S.RtsEnable = true;
+        _portS.ReadTimeout = 10;
+        _portS.WriteTimeout = 10;
+        _portS.DtrEnable = true;
+        _portS.RtsEnable = true;
 
-        port_T.ReadTimeout = 10;
-        port_T.WriteTimeout = 10;
-        port_T.DtrEnable = true;
-        port_T.RtsEnable = true;
+        _portT.ReadTimeout = 10;
+        _portT.WriteTimeout = 10;
+        _portT.DtrEnable = true;
+        _portT.RtsEnable = true;
 
         //OPEN THE PORT
-        port_S.Open();
-        port_T.Open();
+        _portS.Open();
+        _portT.Open();
 
         //ASSIGN GAME OBJECTS
-        sprossen = GameObject.Find("sprossen");
+        sprossen = GameObject.Find("Sprossen 01");
         
-        // Init variable and set current health of persistant interaction (Tree). 
+        // Set initial health of persistant interaction (Tree). 
         _soulHealth = 50;
     }
 
@@ -60,31 +67,31 @@ public class arduinoSerial : MonoBehaviour
     void Update()
     {
         //CHECK SERIAL PORT IS OPEN
-        if (!port_S.IsOpen || !port_T.IsOpen) return;
+        if (!_portS.IsOpen || !_portT.IsOpen) return;
         
         //CHECK IF UNITY HAS RECEIVED ANY COMMUNICATION FROM ARDUINO
-        if (port_S.BytesToRead <= 0) return; 
+        if (_portS.BytesToRead <= 0) return; 
         
         // Collect and parse data from serial connection.
-        var inputStr = port_S.ReadLine().Trim();
+        var inputStr = _portS.ReadLine().Trim();
         var inputInt = int.Parse(inputStr);
 
         //CHANGE COLOUR OF SPRITE BASED ON SPROSSEN MOVEMENT, 0 = PURPLE/NEUTRAL | 1 = GREEN/POSITIVE | 2 = RED/NEGATIVE
         if (inputInt == 0)
         {
             sprossen.GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 0 / 255f, 255 / 255f); //CHANGE COLOUR OF UNITY SPRITE, RGB VALUES, BUT DIVIDE BY 255 AS UNITY ONLY ACCEPTS VALUES BETWEEN 0 AND 1
-            port_T.Write("0");
+            _portT.Write("0");
         } else if (inputInt == 1)
         {
             _soulHealth += 1;
             sprossen.GetComponent<SpriteRenderer>().color = new Color(0 / 255f, 255 / 255f, 0 / 255f);
-            port_T.Write("1");
+            _portT.Write("1");
                     
         } else if (inputInt == 2)
         {
             _soulHealth -= 1;
             sprossen.GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 0 / 255f, 0 / 255f);
-            port_T.Write("2");
+            _portT.Write("2");
         }
     }
 }
