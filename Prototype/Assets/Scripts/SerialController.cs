96972e775469a4fa9470f81eb5903285e2255b07
Treyne11 <54631495+Treyne11@users.noreply.github.com>
2023-04-13 21:30:45 +1000

    Some logic for Controller
    Moving some logic from Arduinos. Setting up different interaction streams for Sprossen, Immediate, and Persistant interactions.

--- Prototype/Assets/Scripts/SerialController.cs
+++ Prototype/Assets/Scripts/SerialController.cs
@@ -5,7 +5,7 @@ using System.IO.Ports;
 
 public class SerialController : MonoBehaviour
 {
-    //INITIALISE SERIAL VARIABLES
+    // INITIALISE SERIAL VARIABLES
     // Init 2 variables for serial data, one for (S)prossen and one for (T)ree
     private SerialPort _portS;
     private SerialPort _portT;
@@ -13,7 +13,7 @@ public class SerialController : MonoBehaviour
     public string portNameT;
     public string[] ports;
 
-    //INITIALISE OTHER VARIABLES
+    // INITIALISE OTHER VARIABLES
     public GameObject sprossen;
     public GameObject leftWall;
     public GameObject rightWall;
@@ -23,25 +23,28 @@ public class SerialController : MonoBehaviour
     
     // Init variable to track health of persistant interaction (Tree). 
     private int _soulHealth;
+    
+    // Init variable to track status of Sprossen (Angry/Happy etc). 
+    private float _sprossenStatus;
 
     // Start is called before the first frame update
     void Start()
     {
-        //START SERIAL COMMUNICATION
+        // START SERIAL COMMUNICATION
         ports = SerialPort.GetPortNames();
 
-        //SET PORT NAMES
-        //COPY AND PASTE TO SET FOR YOUR OWN COMPUTER, COMMENT OUT OTHERS
-        //portName_S = "/dev/cu.usbmodem1413401";
-        //portName_T = "/dev/cu.usbmodem1413301";
+        // SET PORT NAMES
+        // COPY AND PASTE TO SET FOR YOUR OWN COMPUTER, COMMENT OUT OTHERS
+        // portName_S = "/dev/cu.usbmodem1413401";
+        // portName_T = "/dev/cu.usbmodem1413301";
         portNameS = "COM7";
         portNameT = "COM6";
 
-        //SETUP PORT
+        // SETUP PORTS
         _portS = new SerialPort(portNameS, 9600);
         _portT = new SerialPort(portNameT, 9600);
 
-        //SET TIMEOUTS TO LOW NUMBERS TO MAKE COMMUNICATION FAST
+        // SET TIMEOUTS TO LOW NUMBERS TO MAKE COMMUNICATION FAST
         _portS.ReadTimeout = 10;
         _portS.WriteTimeout = 10;
         _portS.DtrEnable = true;
@@ -52,46 +55,68 @@ public class SerialController : MonoBehaviour
         _portT.DtrEnable = true;
         _portT.RtsEnable = true;
 
-        //OPEN THE PORT
+        // OPEN THE PORT
         _portS.Open();
         _portT.Open();
 
-        //ASSIGN GAME OBJECTS
+        // ASSIGN GAME OBJECTS
         sprossen = GameObject.Find("Sprossen 01");
         
         // Set initial health of persistant interaction (Tree). 
         _soulHealth = 50;
+        
+        // Set initial value for status of Sprossen. 
+        _sprossenStatus = 11.0F;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CHECK SERIAL PORT IS OPEN
+        // CHECK SERIAL PORT IS OPEN
         if (!_portS.IsOpen || !_portT.IsOpen) return;
         
-        //CHECK IF UNITY HAS RECEIVED ANY COMMUNICATION FROM ARDUINO
+        // CHECK IF UNITY HAS RECEIVED ANY COMMUNICATION FROM ARDUINO
         if (_portS.BytesToRead <= 0) return; 
         
         // Collect and parse data from serial connection.
         var inputStr = _portS.ReadLine().Trim();
         var inputInt = int.Parse(inputStr);
 
-        //CHANGE COLOUR OF SPRITE BASED ON SPROSSEN MOVEMENT, 0 = PURPLE/NEUTRAL | 1 = GREEN/POSITIVE | 2 = RED/NEGATIVE
-        if (inputInt == 0)
+        // Decay the _sprossenStatus back toward 0 (Neutral)
+        switch (_sprossenStatus)
         {
-            sprossen.GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 0 / 255f, 255 / 255f); //CHANGE COLOUR OF UNITY SPRITE, RGB VALUES, BUT DIVIDE BY 255 AS UNITY ONLY ACCEPTS VALUES BETWEEN 0 AND 1
-            _portT.Write("0");
-        } else if (inputInt == 1)
-        {
-            _soulHealth += 1;
-            sprossen.GetComponent<SpriteRenderer>().color = new Color(0 / 255f, 255 / 255f, 0 / 255f);
-            _portT.Write("1");
-                    
-        } else if (inputInt == 2)
-        {
-            _soulHealth -= 1;
-            sprossen.GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 0 / 255f, 0 / 255f);
-            _portT.Write("2");
+            case > 0:
+                _sprossenStatus -= 0.5F;
+                break;
+            case < 0:
+                _sprossenStatus += 0.5F;
+                break;
         }
+
+        // Update _sprossenStatus with current interaction value. THIS WILL REQ TUNING.
+        _sprossenStatus += inputInt;
+        
+        // CHANGE COLOUR OF SPRITE BASED ON SPROSSEN MOVEMENT, 0 = PURPLE/NEUTRAL, 1 = GREEN/POSITIVE, 2 = RED/NEGATIVE
+        switch (_sprossenStatus)
+        {
+            // CHANGE COLOUR OF SPRITE, DIVIDE RGB VALUES BY 255 AS UNITY ONLY ACCEPTS VALUES BETWEEN 0 AND 1
+            case < 10 and > -10:
+                sprossen.GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 0 / 255f, 255 / 255f); 
+                _portS.Write("0");
+                break;
+            case > 9:
+                _soulHealth += 1;
+                sprossen.GetComponent<SpriteRenderer>().color = new Color(0 / 255f, 255 / 255f, 0 / 255f);
+                _portS.Write("1");
+                break;
+            case < -9:
+                _soulHealth -= 1;
+                sprossen.GetComponent<SpriteRenderer>().color = new Color(255 / 255f, 0 / 255f, 0 / 255f);
+                _portS.Write("2");
+                break;
+        }
+        
+        // Report Persistant Interaction (Tree) Status to 2nd Arduino
+        _portT.Write(_soulHealth.ToString());
     }
 }
